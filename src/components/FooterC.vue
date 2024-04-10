d586f9febd4c311a3830fb2893a36e9082905c72
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-10 18:26:28 +0200

    add links components

--- src/components/FooterC.vue
+++ src/components/FooterC.vue
@@ -1,13 +1,19 @@
 <template>
    <footer>
-        <div id="footer-top" class="d-flex justify-content-around">
-            <div class="p-5">
-                <ul>
-                    <li class="text-white">
-                        <a href=""></a>
-                    </li>
-                </ul>
+        <div id="footer-top" class="d-flex justify-content-around align-items-center ">
+        <div class="d-flex">
+            <div class="px-2">
+                <LinksC :title="links.dccomics.title" :items="links.dccomics.items"/>
+                <LinksC :title="links.shop.title" :items="links.shop.items"/>
             </div>
+            <div class="d-flex">
+                <LinksC :title="links.dc.title" :items="links.dc.items"/>
+                <LinksC :title="links.sites.title" :items="links.sites.items"/>
+            </div>
+        </div>
+        
+           
+
             <div class="logo-bg"></div>
         </div>
         <div id="footer-bottom" class="bg-dark">
@@ -17,11 +23,123 @@
 </template>
 
 <script>
+    import LinksC from './LinksC.vue';
     export default {
         name: 'FooterC',
+        components: {
+            LinksC,
+        },
         data() {
             return {
-                
+                links:{
+                    dccomics:{
+                        title:'DC COMICS',
+                        items:[
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            
+                        ]
+                    },
+                    shop:{
+                        title:'SHOP',
+                        items:[
+                            {
+                                name:'Shop DC',
+                                url:'#'
+                            },
+                            {
+                                name:'Shop DC Collectibles',
+                                url:'#'
+                            },
+                            
+                        ]
+                    },
+                    dc:{
+                        title:'DC',
+                        items:[
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            
+                        ]
+                    },
+                    sites:{
+                        title:'SITES',
+                        items:[
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            },
+                            {
+                                name:'Characters',
+                                url:'#'
+                            }
+                        ]
+                    }
+                }
             }
         }
     }
@@ -37,5 +155,8 @@
         background-position: center;
         background-image: url('/img/dc-logo-bg.png');
     }
-   
+    
+    #footer-bottom{
+        height: 100px;
+    }
 </style>
---
+++ src/components/LinksC.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul class="text-white">
+
+        <h3>{{ title }}</h3>
+        <li class="d-flex flex-column" v-for="(item,index) in items" :key="index">
+            <a :href="item.url">{{item.name}}</a>
+        </li>
+
+    </ul>
+
+</template>
+
+<script>
+    export default {
+        name: 'LinksC',
+        props: ['title', 'items'],
+        data() {
+            return {
+            }
+         }
+    }
+</script>
+
+<style lang="scss" scoped>
+    a:hover{
+        text-decoration: underline;
+    }
+</style>
