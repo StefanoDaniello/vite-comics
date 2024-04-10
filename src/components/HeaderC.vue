93e584efabf32e2bbbfc05cb42572f4d0160e970
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-10 15:34:02 +0200

    add header and main

--- src/components/HeaderC.vue
+++ src/components/HeaderC.vue
@@ -1,15 +1,42 @@
 <template>
-   <header>
-       header
+   <header class="bg-light">
+     <nav class="container d-flex justify-content-between">
+        <div>
+            <img src="../assets/public/img/dc-logo.png" alt="logo"/>
+        </div>
+        <ul class="d-flex">
+            <li v-for="(items,index) in menu" :key="index" class="m-3">
+                {{items}}
+            </li>
+        </ul>
+     </nav>
    </header>
 </template>
 
 <script>
     export default {
         name: 'HeaderC',
+        data() {
+            return {
+                menu:[
+                    'CHARACTERS',
+                    'COMICS',
+                    'MOVIES',
+                    'TV',
+                    'GAMES',
+                    'COLLECTIBLES',
+                    'VIDEOS',
+                    'FANS',
+                    'NEWS',
+                    'SHOP'
+                ]
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-
+   ul{
+        list-style: none;
+    }
 </style>
--- src/components/MainC.vue
+++ src/components/MainC.vue
@@ -1,15 +1,60 @@
 <template>
    <main>
-    main
+    <div class="bg-black">
+        <div class="container p-5">
+           <span class="bg-black text-white">--> Content goes here <--</span>
+        </div>
+    </div>
+        <div class="bg-info ">
+            <div class="container">
+                <ul class="d-flex justify-content-center text-white">
+                    <li v-for="(img,index) in shopimg" :key="index" class="m-3">
+                        <div>
+                            <img :src="img" alt=""/>
+                        </div>
+                    </li>
+                    <li v-for="(items,index) in shop" :key="index" class="m-3">
+                        {{items}}
+                    </li>
+                </ul>
+            </div>
+        </div>
    </main>
 </template>
 
 <script>
     export default {
         name:'MainC',
+        data() {
+            return {
+                shop:[
+                    'DIGITAL COMICS',
+                    'DC MERCHANDISE',
+                    'SUBSCRIPTION',
+                    'COMIC SHOP LOCATOR',
+                    'DC POWER VISA'
+                ],
+                shopimg:[
+                    '../assets/public/img/buy-comics-digital-comics.png',
+                    '../assets/public/img/buy-comics-merchandise.png',
+                    '../assets/public/img/buy-comics-subscriptions.png',
+                    '../assets/public/img/buy-comics-shop-locator.png',
+                    '../assets/public/img/buy-dc-power-visa.svg'
+                ]
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-
+    ul{
+        list-style: none;
+    }
+    span{
+        font-size: 25px;
+        font-weight: bold;
+    }
+    img{
+        width: 100px;
+    }
 </style>
