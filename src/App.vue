e6205673eba32e3746443e09ef3e6d5334af9ebe
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-10 14:48:15 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <HeaderC/>
+  <MainC/>
+  <FooterC/>
+</template>
+
+<script>
+import HeaderC from './components/HeaderC.vue';
+import MainC from './components/MainC.vue';
+import FooterC from './components/FooterC.vue';
+ 
+export default {
+    name: 'App',
+    components: {
+        HeaderC,
+        MainC,
+        FooterC,
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
